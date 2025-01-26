e0d0e42b199eb542271daaf0f93c44cc51a70eb3
Willian <williansilvaoliveira1234@example.com>
2025-01-26 20:24:00 -0300

    first commit

---
+++ js/js2.js
@@ -0,0 +1,102 @@
+var input = document.getElementById("input")
+var btn = document.getElementById("btn")
+var caixa2 = document.getElementById("caixa2")
+ var btn2 = document.getElementById("btn2")
+
+
+
+
+btn2.addEventListener("click",()=>{
+	
+	const all = [...document.querySelectorAll(".div1")]
+	
+	all.map((el)=>{
+		
+		el.remove()
+		
+	})
+	
+	
+	
+		
+	
+}) 
+
+	var remove = ()=>{
+		
+		var color = [...document.querySelectorAll(".back")]
+		
+		color.map((el)=>{
+			
+		el.classList.remove("back")
+			
+		})
+		
+	}
+
+var course = ()=>{
+	
+	
+	
+	
+	
+	var div  = document.createElement("div")
+	div.setAttribute("class","div1")
+	div.addEventListener("click",(evt)=>{
+
+	remove()
+	
+	evt.target.classList.toggle("back")
+		
+	})
+	div.innerHTML  = input.value
+	
+	
+
+		
+	
+		
+
+	
+	caixa2.appendChild(div)
+	
+	
+	 
+	 input.focus()
+	
+	
+	if(input.value === ""){
+		
+		div.remove()
+		
+	
+		}
+	
+	
+
+				
+	 
+
+
+
+}
+
+
+	btn.addEventListener("click",(evt)=>{
+		
+	
+	  course()
+	  
+	  console.log(evt.target)
+		
+				input.value = ""
+
+		
+	})
+	
+	
+
+
+	
+	
+	
